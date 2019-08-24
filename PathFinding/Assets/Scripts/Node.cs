2509ea4ffa247c6e466cf7f6ce81042a8f927819
nicolas <nicolaspiccitto@gmail.com>
2019-08-23 21:36:39 -0300

    PathFinding-v0.2
    scripts created

---
+++ PathFinding/Assets/Scripts/Node.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum NodeState
+{
+    ready = 0,
+    opened,
+    closed
+}
+public class Node 
+{
+ private List<Node> _adjNodes = new List<Node>();
+ bool _isObstacle = false;
+ private Vector3 _position;
+
+  
+}
---
+++ PathFinding/Assets/Scripts/NodeGenerator.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeGenerator 
+{
+//tirar raycast en un doble for para calcular cada nodo.
+//para calcular los nodos adyasentes tirar un ray para todos lados.
+//usar .hit point para saber la pos del nodo.
+}
